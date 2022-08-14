05894a01db04eca049fc40540ff8771d64b25e1b
joyjprince <joyjprince@gmail.com>
2022-08-14 20:18:05 +0530

    Initial Commit

---
+++ src/main/java/com/prince/jsondatacreator/JsonDataCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.prince.jsondatacreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonDataCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonDataCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prince/jsondatacreator/JsonDataCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prince.jsondatacreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonDataCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
