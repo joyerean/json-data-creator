8b705eff99447db1e0957628a19b5b331d77b0fd
joyerean <joyjprince@gmail.com>
2022-08-25 03:22:50 +0530

    Controller changes

--- src/main/java/com/prince/jsondatacreator/api/JsonModelController.java
+++ src/main/java/com/prince/jsondatacreator/api/JsonModelController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prince.jsondatacreator.model.JsonModel;
 import com.prince.jsondatacreator.service.JsonModelService;
 
-@RestController(value = "/api/jsonModel")
+@RestController
+@RequestMapping("/api/jsonModel")
 public class JsonModelController {
 	
 	@Autowired
